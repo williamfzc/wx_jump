4b41eb8f8521574a0aeb47e7c4f50ecf7f2a68dd
williamfzc <178894043@qq.com>
2017-12-30 14:56:44 +0800

    bug fix and 能跳50步了..

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-from PIL import Image, ImageStat
+from PIL import Image, ImageStat, ImageFilter, ImageDraw
 import os
 import numpy as np
 import time
@@ -12,7 +12,7 @@ def get_pic(_pic_path):
 
 
 def calculate_time(dis):
-    return int(dis * 710 / 436.165)
+    return int(dis * 710 / 482.44792465094093)
 
 
 def analyse_pic(_pic_path):
@@ -21,10 +21,12 @@ def analyse_pic(_pic_path):
         get_pic('temp.png')
 
         img = Image.open(_pic_path)
-        img = img.crop((0,0, 1080,1050))
+
 
         # get self location
         self_x, self_y = get_self_position(img)
+
+        img = img.crop((0, 0, 1080, 900))
 
         # get des location
         des_x, des_y = get_des_position(img)
@@ -61,7 +63,7 @@ def get_self_position(img):
 
     # return sum([each[0] for each in point_list])/len(point_list),\
     #        sum([each[1] for each in point_list])/len(point_list)
-    return point_list[-1][0], point_list[-1][1]
+    return point_list[-1][0], point_list[-1][1]+50
 
 
 def rgb_compare(a, b):
@@ -74,35 +76,64 @@ def rgb_compare(a, b):
         return True
 
 
+def depoint(img):
+    pixdata = img.load()
+    w,h = img.size
+    for y in range(1,h-1):
+        for x in range(1,w-1):
+            count = 0
+            if pixdata[x,y-1] < 15:
+                count = count + 1
+            if pixdata[x,y+1] < 15:
+                count = count + 1
+            if pixdata[x-1,y] < 15:
+                count = count + 1
+            if pixdata[x+1,y] < 15:
+                count = count + 1
+            if count > 2:
+                pixdata[x, y] = 0
+    return img
+
+
 def get_des_position(img):
+    img = img.filter(ImageFilter.FIND_EDGES)
     # 灰度图
     img = img.convert('L')
-    # Laplace算子
-    suanzi1 = np.array([[0, 1, 0],
-                        [1, -4, 1],
-                        [0, 1, 0]])
-    img = signal.convolve2d(img, suanzi1, mode="same")
-    img = (img / float(img.max())) * 255
-    img[img > img.mean()] = 255
-    img[img <= img.mean()] = 0
+    #
+    img = depoint(img)
+
+    img = np.array(img)
+    img[img > 2] = 255
+    img[img <= 2] = 0
+
+    # img = Image.fromarray(img)
+    # img.save('temp1.png')
 
     _row = 0
     for index, row in enumerate(img[::-1]):
-        if list(row).count(0) == 4:
+        if list(row).count(255) in range(2, 8):
             _row = index
             break
-    if _row > 500:
-        for index, row in enumerate(img[::-1]):
-            if list(row).count(0) == 5:
-                _row = index
-                break
-    des_y = 1050 - _row
+    # if _row > 400:
+    #     for index, row in enumerate(img[::-1]):
+    #         if list(row).count(255) in range(3, 8):
+    #             _row = index
+    #             break
+    des_y = 900 - _row
 
     des_row = list(img[des_y])[1:-1]
-    des_x = (des_row.index(0) + len(des_row) - des_row[::-1].index(0)) / 2
+    front = des_row.index(255)
+    back = len(des_row) - des_row[::-1].index(255)
+    if back - front < 50:
+        des_x = des_row.index(255) + 210
+    else:
+        des_x = (front + back) / 2
+    # des_x = des_row.index(255) + 210
 
     img = Image.fromarray(img)
-    img.show()
+    draw = ImageDraw.Draw(img)
+    draw.arc((des_x, des_y, des_x + 20, des_y + 20), 0, 360, fill=150)
+    img.save('temp1.png')
 
     return des_x, des_y
 
