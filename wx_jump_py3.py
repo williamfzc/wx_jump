e86fe7a001ebcdb35a2ee7fb43a8b72bf5bee546
williamfzc <178894043@qq.com>
2018-01-02 10:06:06 +0800

    somefix

--- wx_jump_py3.py
+++ wx_jump_py3.py
@@ -11,7 +11,7 @@ import time
 # 例如，我运行第一遍算出来的distance为562.5，记录到的XXX为720
 # 那么此处的DISTANCE_ARG 为 720/562.5 = 1.28
 # 还没在很多机型上试过，后期会将该过程封装起来，目前大概是这么调整
-DISTANCE_ARG = 1.382
+DISTANCE_ARG = 1.383
 # 设备型号
 DEVICE_SCREEN = (1080, 1920)
 # 每次跳的停等时间，如果前期纪录较低建议设为2以防止“超越”字样的影响
@@ -133,7 +133,7 @@ def _get_des_y(_cur_row, _des_x, _img):
                     continue
                 else:
                     _result = _get_des_x(each_col, _old_line)
-                    return (_result + _cur_row) / 2 + IGNORE_HEIGHT[0]
+                    return int((_result + _cur_row) / 2) + IGNORE_HEIGHT[0]
 
 
 def fix_distance(_self_point, _des_point, _origin_dis):
